3e4103ead47c77cf98c714d9881bd1f12ffee85c
Vladislav Kashpersky <vlakas1978@yandex.ru>
2022-04-23 14:50:17 +0300

    Modify main

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,2 +1,13 @@
 package ru.netology
 
+fun main(){
+
+    val moneyTransfer = 10000
+    val commision =  0.75 / 100
+    val minCommission = 3500
+
+    val amount = if (moneyTransfer * commision < minCommission)
+        minCommission else moneyTransfer * commision
+
+    println("Ваша комиссия составить: $amount  копеек")
+}
